befb7ff2d021ad344cf5a61e5a76506799f80b57
Pavel Penev <pavpen@gmail.com>
2018-03-28 01:34:05 -0400

    Periodic commit with Bézier linearization.

--- include/cpp_cad/Spline/CubicBezierSegmentLinearizer.h
+++ include/cpp_cad/Spline/CubicBezierSegmentLinearizer.h
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <vector>
 
+#include "../algebra/vector_3.h"
 #include "CubicBezierSegment.h"
 
 
@@ -25,6 +26,11 @@ class CubicBezierSegmentLinearizer
 
     const CubicBezierSegment<PointType> &segment;
 
+    // `max_lateral_distance` = The `f` parameter in the Hain, Ahmad, Langan
+    //         paper.  I.e. an estimate of the maximum (lateral) distance of a
+    //         line segment from the curve.
+    double max_lateral_distance;
+
     // The Bézier basis matrix.  If multiplied by [t^3, t^2, t, 1], it gives
     // the location of the point on the Bézier curve at parameter t.
     double a_x, b_x, c_x, d_x;
@@ -34,6 +40,15 @@ class CubicBezierSegmentLinearizer
     // Values of the `t` parameter at which inflection points occur.  (I.e.,
     // when the velocity and the acceleration vectors become collinear.)
     double t_inflect1, t_inflect2;
+
+    // Value (`t_f` in Hain, Ahmad, and Langan) used when estimating the error
+    // around inflection points.
+    double t_f;
+
+    // Values of `t` at which line segments for each inflection point start
+    // and end.
+    double t_inflect1_start, t_inflect1_end;
+    double t_inflect2_start, t_inflect2_end;
 
     // When two floating-point values are closer to each other than `eps`,
     // they are considered equal (enough).
@@ -51,29 +66,418 @@ class CubicBezierSegmentLinearizer
     //     * No cusps: t_cusp1 = NaN, t_cusp2 = NaN.
     double t_cusp1, t_cusp2;
 
+    // The orthonormal basis vectors in the coordinate system that travels
+    // along the curve segment.  The `q` axis (`r` in Hain, Ahmad, Langan) is
+    // in the direction from the first control point to the second.  The
+    // `r` axis (`s` in Hain, Ahmad, Langan) is orthogonal to `q` and in the
+    // plane of the first 3 control points.  The `s` axis forms a right-handed
+    // coordinate system with `q` and `r`.
+    double q_x, q_y, q_z;
+    double r_x, r_y, r_z;
+    double s_x, s_y, s_z;
+
     public:
 
     CubicBezierSegmentLinearizer(
-        const CubicBezierSegment<PointType> &segment, double eps = 1e-15)
+        const CubicBezierSegment<PointType> &segment,
+        double max_lateral_distance, double eps = 1e-15)
     : segment(segment),
+        max_lateral_distance(max_lateral_distance),
         eps(eps)
     {}
 
     // Returns the values of the `t` parameter for the points on the curve
     // that can be connected by line segments.
-    //
-    // `max_lateral_distance` = The `f` parameter in the Hain, Ahmad, Langan
-    //         paper.  I.e. an estimate of the maximum (lateral) distance of a
-    //         line segment from the curve.
-    std::vector<double> linearize_ts(double max_lateral_distance)
+    std::vector<double> linearize_ts()
     {
         calculate_t_basis_matix();
         calculate_cusp_ts();
         calculate_inflection_ts();
-        throw "TODO: Not implemented!";
+        calculate_inflect_line_segments();
+        calculate_qrs_basis();
+
+        // Split the curve at cusp points, use line segments around inflection
+        // points, and subdivide the rest using parabolic error approximation.
     }
 
     private:
+
+    // Requires inflection line segments, `q`-`r`-`s` basis, and `t_f` to have
+    // been calculated.  Only works in the [0; 1] range.
+    template <class OutputIterator>
+    inline OutputIterator &linearize_cusp_free_range(double t_start, double t_end,
+        OutputIterator &output_iter)
+    {
+        // We have to perform parabolic linerization only on ranges that don't
+        // intersect inflection point line segment ranges.
+        linearize_cusp_free_range_to_inflection(
+            t_start, t_end, t_inflect1_start, t_inflect1_end, output_iter);
+
+        if (std::isnan(t_start))
+        {
+            return;
+        }
+
+        linearize_cusp_free_range_to_inflection(
+            t_start, t_end, t_inflect2_start, t_inflect2_end, output_iter);
+
+        if (std::isnan(t_start))
+        {
+            return;
+        }
+
+        return parabolic_linearize_range(t_start, t_end, output_iter);
+    }
+
+    template <class OutputIterator>
+    inline void linearize_cusp_free_range_to_inflection(
+        double &t_start, double &t_end, double t_inflect_start, double t_inflect_end,
+        OutputIterator &output_iter)
+    {
+        if (std::isnan(t_inflect_start) || t_end <= t_inflect_start + eps)
+        {
+            return output_iter;
+        }
+
+        if (t_start + eps < t_inflect_end)
+        {
+            // Inflection point range intersects [t_start; t_end].
+            if (t_start + eps < t_inflect_start)
+            {
+                // Linearize to inflection range start:
+                output_iter = parabolic_linearize_range(
+                    t_start, t_inflect_start, output_iter);
+            }
+            if (t_end <= t_inflect_end + eps)
+            {
+                // t_end is in the inflection range:
+                (*output_iter) = t_end;
+                ++output_iter;
+
+                t_start = NAN;
+
+                return output_iter;
+            }
+            // Line segment to the end of inflection range 1:
+            (*output_iter) = t_inflect_end;
+            ++output_iter;
+            // Continue from t_inflect_end:
+            t_start = t_inflect_end;
+        }
+    }
+
+    template <class OutputIterator>
+    inline OutputIterator &parabolic_linearize_range(double t_start, double t_end,
+        OutputIterator &output_iter)
+    {
+        if (t_end - t_start < eps)
+        {
+            return output_iter;
+        }
+
+        double t;
+
+        if (t_start <= eps)
+        {
+            t = parabolic_split_t();
+        }
+        if (t + eps >= t_end)
+        {
+            (*output_iter) = t_end;
+            ++output_iter;
+
+            return output_iter;
+        }
+        (*output_iter) = t;
+        ++output_iter;
+        t_start = t;
+
+        CubicBezierSegmentLinearizer
+            linearizer(segment, max_lateral_distance, eps);
+
+        while (true)
+        {
+            CubicBezierSegment sub_segment =
+                split_end_segment_from_t(t_start);
+
+            linearizer.segment = sub_segment;
+            linearizer.calculate_qrs_basis();
+            t = linearizer.parabolic_split_t();
+
+            if (t + eps >= t_end)
+            {
+                (*output_iter) = t_end;
+                ++output_iter;
+
+                return output_iter;
+            }
+            (*output_iter) = t;
+            ++output_iter;
+            t_start = t;
+        }
+    }
+
+    // Returns a cubic Bézier segment that coincides with the piece of this
+    // Bézier segment from `t` = `t_cut` to `t` = 1.
+    //
+    // The `t` of the new segment is `t_2 = (t - t_cut) / (1 - t_cut)`,
+    // where `t` is the `t` of this segment.
+    CubicBezierSegment split_end_segment_from_t(double t_cut)
+    {
+        std::tuple<double, double, double>
+            p0 = vector_3::point_to_tuple(segment.p0()),
+            p1 = vector_3::point_to_tuple(segment.p1()),
+            p2 = vector_3::point_to_tuple(segment.p2()),
+            p3 = vector_3::point_to_tuple(segment.p3()),
+            p0_prime = p0 + (p1 - p0) * t_cut,
+            p1_prime = p1 + (p2 - p1) * t_cut,
+            p2_prime = p2 + (p3 - p2) * t_cut,
+            p0_double_prime = p0_prime + (p1_prime - p0_prime) * t_cut,
+            p1_double_prime = p1_prime + (p2_prime - p1_prime) * t_cut,
+            p0_triple_prime =
+                p0_double_prime + (p1_double_prime - p0_double_prime) * t_cut;
+
+        return CubicBezierSegment(
+            PointType(
+                std::get<0>(p0_triple_prime),
+                std::get<1>(p0_triple_prime),
+                std::get<2>(p0_triple_prime) ),
+            PointType(
+                std::get<0>(p1_double_prime),
+                std::get<1>(p1_double_prime),
+                std::get<2>(p1_double_prime) ),
+            PointType(
+                std::get<0>(p2_prime),
+                std::get<1>(p2_prime),
+                std::get<2>(p2_prime) ),
+            PointType(
+                std::get<0>(p3),
+                std::get<1>(p3),
+                std::get<2>(p3) )
+        );
+    }
+
+    // Return the `t` parameter at which to split the curve from the
+    // beginning, so that it's distance from a line segment is no more than
+    // about `max_lateral_distance`.
+    //
+    // Requires the `q`, `r`, `s` basis to have been calculated.
+    inline double parabolic_split_t()
+    {
+        double r2 = vector_3::dot_3(
+            segment.p2().x(), segment.p2().y(), segment.p2().z(),
+            r_x, r_y, r_z);
+        double s2 = vector_3::dot_3(
+            segment.p2().x(), segment.p2().y(), segment.p2().z(),
+            s_x, s_y, s_z);
+
+        return 2 * sqrt( max_lateral_distance / 3 * sqrt(r2 * r2 + s2 * s2) );
+    }
+
+    inline void calculate_inflect_line_segments()
+    {
+        if (std::isnan(t_inflect1))
+        {
+            if (std::isnan(t_inflect2))
+            {
+                t_inflect1_start = t_inflect1_end = NAN;
+            }
+            else
+            {
+                std::tie(t_inflect1_start, t_inflect1_end) =
+                    inflection_split_t_range(t_inflect2);
+            }
+            t_inflect2_start = t_inflect2_end = NAN;
+        }
+        else
+        {
+            std::tie(t_inflect1_start, t_inflect1_end) =
+                inflection_split_t_range(t_inflect1);
+            if (std::isnan(t_inflect2))
+            {
+                t_inflect2_start = t_inflect2_end = NAN;
+                return;
+            }
+            if (std::isnan(t_inflect1_start))
+            {
+                std::tie(t_inflect1_start, t_inflect1_end)
+                    = inflection_split_t_range(t_inflect2);
+                t_inflect2_start  = t_inflect2_end = NAN;
+                return;
+            }
+            if (!std::isnan(t_inflect2))
+            {
+                std::tie(t_inflect2_start, t_inflect2_end) =
+                    inflection_split_t_range(t_inflect2);
+                if (std::isnan(t_inflect2_start))
+                {
+                    return;
+                }
+                if (t_inflect2_start < t_inflect1_end)
+                {
+                    // The two ranges overlap.
+                    if (t_inflect2_start <= t_inflect1_start + eps)
+                    {
+                        // Range 2 includes range 1.
+                        t_inflect1_start = t_inflect2_start;
+                        t_inflect1_end = t_inflect2_end;
+                        t_inflect1_start = NAN;
+                        t_inflect1_end = NAN;
+                        return;
+                    }
+                    if (t_inflect1_end + eps >= t_inflect2_end)
+                    {
+                        // Range 1 includes range 2.
+                        t_inflect2_start = NAN;
+                        t_inflect2_end = NAN;
+                        return;
+                    }
+                    t_inflect1_end = t_inflect2_start =
+                        (t_inflect1_end + t_inflect2_start) / 2;
+                }
+            }
+        }
+    }
+
+    // Requires `t_f` to have been calculated.
+    inline std::tuple<double, double> inflection_split_t_range(
+        double t_inflection)
+    {
+        double t_start = t_inflection - t_f * (1 - t_inflection);
+        double t_end   = t_inflection + t_f * (1 - t_inflection);
+
+        if (t_start < 0)
+        {
+            t_start = 0;
+        }
+        else if (t_start >= 1)
+        {
+            return std::make_tuple(NAN, NAN);
+        }
+
+        if (t_end <= 0)
+        {
+            return std::make_tuple(NAN, NAN);
+        }
+        else if (t_end > 1)
+        {
+            t_end = 1;
+        }
+
+        return std::make_tuple(t_start, t_end);
+    }
+
+    // Requires the `q`, `r`, `s` basis to have been calculated.
+    inline void calculate_t_f()
+    {
+        double r3 = dot_3(
+            segment.p3().x(), segment.p3().y(), segment.p3().z(),
+            r_x, r_y, r_z);
+        double s3 = dot_3(
+            segment.p3().x(), segment.p3().y(), segment.p3().z(),
+            s_x, s_y, s_z);
+
+        t_f = cbrt(max_lateral_distance / sqrt(r3 * r3 + s3 * s3));
+    }
+
+    void calculate_qrs_basis()
+    {
+        if (distance_3_squared(p0, p1) < eps)
+        {
+            calculate_qrs_basis(p0, p2, p3);
+        }
+        else if (distance_3_squared(p0, p2) < eps)
+        {
+            calculate_qrs_basis(p0, p1, p3);
+        }
+        else if (distance_3_squared(p0, p3) < eps)
+        {
+            calculate_qrs_basis(p0, p1, p2);
+        }
+        else if (distance_3_squared(p1, p2) < eps)
+        {
+            calculate_qrs_basis(p0, p1, p3);
+        }
+        else
+        {
+            calculate_qrs_basis(p0, p1, p2);
+        }
+    }
+
+    // Construct `q`, `r` and `s` basis vectors from 3 points,
+    // `p0`, `p1` and `p2`.
+    inline void calculate_qrs_basis(
+        const PointType &p0, const PointType &p1, const PointType &p2)
+    {
+        if (distance_3_squared(p0, p1) < eps)
+        {
+            calculate_qrs_basis(p0, p2);
+            return;
+        }
+        else if (distance_3_squared(p0, p2) < eps ||
+            distance_3_squared(p1, p2) < eps)
+        {
+            calculate_qrs_basis(p0, p1);
+            return;
+        }
+
+        q_x = p1.x() - p0.x();
+        q_y = p1.y() - p0.y();
+        q_z = p1.z() - p0.z();
+
+        double norm = sqrt(norm_3_squared(q_x, q_y, q_z));
+
+        q_x /= norm;
+        q_y /= norm;
+        q_z /= norm;
+
+        r_x = p2().x - p0.x();
+        r_y = p2().y - p0.y();
+        r_z = p2().z - p0.z();
+        norm = sqrt(norm_3_squared(r_x, r_y, r_z));
+        r_x /= norm;
+        r_y /= norm;
+        r_z /= norm;
+
+        std::tie(s_x, s_y, s_z) = cross_3(q_x, q_y, q_z, r_x, r_y, r_z);
+        std::tie(r_x, r_y, r_z) = cross_3(s_x, s_y, s_z, q_x, q_y, q_z);
+    }
+
+    // Construct `q`, `r` and `s` basis vectors from only 2 points,
+    // `p0`, and `p1`.
+    inline void calculate_qrs_basis(const PointType &p0, const PointType &p1)
+    {
+        if (distance_3_squared(p0, p1) < eps)
+        {
+            calculate_qrs_basis(p0);
+            return;
+        }
+
+        q_x = p1.x() - p0.x();
+        q_y = p1.y() - p0.y();
+        q_z = p1.z() - p0.z();
+
+        double norm = sqrt(norm_3_squared(q_x, q_y, q_z));
+
+        q_x /= norm;
+        q_y /= norm;
+        q_z /= norm;
+
+        std::tie(r_x, r_y, r_z) = find_perpendicular_axis(q_x, q_y, q_z);
+
+        std::tie(s_x, s_y, s_z) = cross_3(q_x, q_y, q_z, r_x, r_y, r_z);
+        std::tie(r_x, r_y, r_z) = cross_3(s_x, s_y, s_z, q_x, q_y, q_z);
+    }
+
+    // Construct some `q`, `r` and `s` basis vectors when all control points
+    // concide.
+    inline void calculate_qrs_basis(const PointType &)
+    {
+        // All points coincide.  Use x, y, z coordinates:
+        q_x = 1; q_y = 0; q_z = 0;
+        r_x = 0; r_y = 1; r_z = 0;
+        s_x = 0; s_y = 0; s_z = 1;
+    }
 
     void calculate_t_basis_matix()
     {
@@ -188,6 +592,8 @@ class CubicBezierSegmentLinearizer
 
     // Calculates inflection points in the [0; 1] range and sets
     // `t_inflect1` and `t_inflect2` to them.
+    //
+    // Requires the Bézier basis matrix to have been calculated.
     void calculate_inflection_ts()
     {
         double t1, t2;
---
+++ include/cpp_cad/algebra/vector_3.h
@@ -0,0 +1,105 @@
+#ifndef _CPP_CAD_ALGEBRA_VECTOR_3_H
+#define _CPP_CAD_ALGEBRA_VECTOR_3_H
+
+#include <tuple>
+
+
+namespace cpp_cad
+{
+
+namespace vector_3
+{
+
+template <class PointType>
+inline std::tuple<double, double, double> point_to_tuple(const PointType &p)
+{
+    return std::make_tuple(p.x(), p.y(), p.z());
+}
+
+inline std::tuple<double, double, double> operator+(
+    const std::tuple<double, double, double> &a,
+    const std::tuple<double, double, double> &b)
+{
+    return std::make_tuple(
+        std::get<0>(a) + std::get<0>(b),
+        std::get<1>(a) + std::get<1>(b),
+        std::get<2>(a) + std::get<2>(b) );
+}
+
+inline std::tuple<double, double, double> operator-(
+    const std::tuple<double, double, double> &a,
+    const std::tuple<double, double, double> &b)
+{
+    return std::make_tuple(
+        std::get<0>(a) - std::get<0>(b),
+        std::get<1>(a) - std::get<1>(b),
+        std::get<2>(a) - std::get<2>(b) );
+}
+
+inline std::tuple<double, double, double> operator*(
+    const std::tuple<double, double, double> &a,
+    double b)
+{
+    return std::make_tuple(
+        b * std::get<0>(a),
+        b * std::get<1>(a),
+        b * std::get<2>(a) );
+}
+
+inline double norm_3_squared(double x, double y, double z)
+{
+    return x*x + y*y + z*z;
+}
+
+inline std::tuple<double, double, double> cross_3(
+    double x1, double y1, double z1,
+    double x2, double y2, double z2)
+{
+    return std::make_tuple(
+        y1 * z2 - z1 * y2,
+        z1 * x2 - x1 * z2,
+        x1 * y2 - y1 * x2
+    );
+}
+
+double dot_3(
+    double x1, double y1, double z1,
+    double x2, double y2, double z2)
+{
+    return x1 * x2 + y1 * y2 + z1 * z2;
+}
+
+// Returns some unit vector orthogonal to a given vector.
+inline std::tuple<double, double, double> find_perpendicular_axis(
+    double x, double y, double z)
+{
+    if (abs(x) <= abs(y) && abs(x) <= abs(z))
+    {
+        // Use `res` = `input` cross `x`:
+        return std::make_tuple(0, z, -y);
+    }
+    else if (abs(y) <= abs(x) && abs(y) <= abs(z))
+    {
+        // Use `res` = `input` cross `y`:
+        return std::make_tuple(-z, 0, -y);
+    }
+    else
+    {
+        // Use `res` = `input` cross `z`:
+        return std::make_tuple(y, -x, 0);
+    }
+}
+
+// Returns the squared distance between two points.
+template <class PointType>
+inline double distance_3_squared(const PointType &p0, const PointType &p1)
+{
+    return norm_3_squared(
+        p1.x() - p0.x(), p1.y() - p0.y(), p1.z() - p0.z());
+}
+
+}
+
+}
+
+#endif // _CPP_CAD_ALGEBRA_VECTOR_3_H
